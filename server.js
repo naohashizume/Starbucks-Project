a4ccf7ce11556ebb9acbc951b284f157c052971b
henryn1996 <32000477+henryn1996@users.noreply.github.com>
2018-05-18 18:01:29 -0700

    fixed error when favourites would not pop up after searching for blank location (#80)
    * Initial commit
    
    * added project to github
    
    * initialising the project directory
    
    * Documented the maps.js
    
    * Documented the maps.js
    
    * Geting location based on IP is added with all docs
    
    * added some docstrings and fixed fav function
    
    * hi
    
    * hi there
    
    * hi
    
    * This is test
    
    * This is the master changes
    
    * fixed a logo
    
    * This is a test
    
    * hi
    
    * added stuff to server and user error page
    
    * Update accounts.json
    
    * Added encryption
    
    * Revert "Added encryption"
    
    * Added encryption
    
    * stuff
    
    * autoformatted a bunch of files and deleted useless text file.
    
    * added documentation for server.js
    
    * jsdoc html for server
    
    * made login form more attractive
    
    * made login form more attractive
    
    * Deleted index3.hbs, made favourites list into divs and styled them, fixed bug where empty locations could be saved
    
    * fixed create acc form
    
    * Update accounts.json
    
    * Added favicon
    
    * Update i2.js
    
    * styled map page and increased transition speed.
    
    * changed styles
    
    * Overhauled account creation prompt
    
    * Update i2.js
    
    *  min user/pass, need to change error page
    
    * added a slide show to background
    
    * changed some transitions
    
    * added gitignore
    
    * added test.js
    
    * login unit test
    
    * removed a space
    
    * this is google maps
    
    * fxed when saving pops up 2 saved msgs
    
    * Working Google maps
    
    * Login tests
    
    * Update test.js
    
    * removed <ul> in server.js
    
    * Update style.css
    
    * This is google maps with the current ip
    
    * Get map work
    
    * Fixed a bug with populating map
    
    * finished feature 1
    
    * Now it works with search in other cities
    
    * Update maps.js
    
    * added error popup to search
    
    * Fixed zoom
    
    * Added places.json and accounts.json to .gitignore
    
    * fixed git ignore
    
    * .gitignore
    
    * TEST
    
    * Fixed transitions for submit button, and updated readme.
    
    * The zoom bug fixed
    
    * The zoom bug fixed
    
    * formatting
    
    * package lock json
    
    * added popups to favourite page
    
    * created test sql database file to test database
    
    * sql successfully setup
    
    * testing database file
    
    * Update server.js
    
    * rendering fixed fav bug
    
    * fixed rendering favourites bug
    
    * cannot search empty values anymore
    
    * Update server.js
    
    * fixed merge
    
    * fixed error where favourites would not render

--- server.js
+++ server.js
@@ -42,11 +42,13 @@ var con = mysql.createConnection({
 /**
  * Takes user's favorites list and Emails it to user
  */
+
 var send_mail = () => {   
     options = email.mailOptions;
     options.to = 'viktor.sheverdin@gmail.com';
     options.subject = 'Test email from Sb app';
     options.text = 'OK! It actually works!';
+
     console.log(options);
     email.send_email(options);
 
@@ -73,7 +75,7 @@ var send_mail = () => {
  */
 var LoadAccfile = () => {
     return new Promise(resolve => {
-        con.query('SELECT * FROM users', function (err, res, fields) {
+        con.query('SELECT * FROM users', function(err, res, fields) {
             resolve(Accs = JSON.parse(JSON.stringify(res)));
         });
     });
@@ -86,7 +88,7 @@ var LoadAccfile = () => {
 var loadUserdata = (user) => {
     return new Promise(resolve => {
         console.log(user);
-        con.query("SELECT * from UserData WHERE username = '" + user + "'", function (err, res, fields) {
+        con.query("SELECT * from UserData WHERE username = '" + user + "'", function(err, res, fields) {
             //console.log(res)
             resolve(saved_loc = JSON.parse(JSON.stringify(res)));
         });
@@ -99,9 +101,9 @@ var loadUserdata = (user) => {
  * @param {string} location - Is the location address the user is trying to save
  */
 var checkLocations = (user, location) => {
-    return new Promise(function (resolve, reject) {
+    return new Promise(function(resolve, reject) {
         console.log("SELECT * from UserData WHERE username ='" + user + "' AND location_id = '" + location + "'");
-        con.query("SELECT * from UserData WHERE username ='" + user + "' AND location_id = '" + location + "'", function (err, res, fields) {
+        con.query("SELECT * from UserData WHERE username ='" + user + "' AND location_id = '" + location + "'", function(err, res, fields) {
             var loc = JSON.stringify(res);
             console.log(loc);
             if (loc == '[]') {
@@ -153,20 +155,20 @@ var Login = (request, response) => {
                             testvar: displayText,
                             coord: `<script>latitude = ${response1.lat}; longitude = ${response1.lon};initMultPlaceMap()</script>`
                         });
+                        // response.render('index2.hbs', {
+                        //     savedSpots: displaySaved,
+                        //     coord: `<script>latitude = ${response.lat}; longitude = ${response.lon};defMap()</script>`
+                        // })
                     });
-                    // response.render('index2.hbs', {
-                    //     savedSpots: displaySaved,
-                    //     coord: `<script>latitude = ${response.lat}; longitude = ${response.lon};defMap()</script>`
-                    // })
                 });
-            });
-        },
+            },
             rej => {
                 response.render('index.hbs', {
                     username: 3
                 });
             }
         );
+    });
     });
 };
 
@@ -176,7 +178,7 @@ var Login = (request, response) => {
  * @param {string} accs - The list object passed in from Login fucntion
  */
 var LoginCheck = (request, accs) => {
-    return new Promise(function (resolve, reject) {
+    return new Promise(function(resolve, reject) {
         for (i = 0; i < accs.length; i++) {
             //console.log(accs[i].username, request.body.username)
             console.log(accs[i].salt);
@@ -206,7 +208,7 @@ var AddUsr = (request, response) => {
                 'user': request.body.NewUser,
                 'pass': hash_password,
             };
-            con.query("INSERT INTO users (username, pass, salt) values ('" + acc.user + "','" + acc.pass + "','" + salt + "')", function (err, res, fields) {
+            con.query("INSERT INTO users (username, pass, salt) values ('" + acc.user + "','" + acc.pass + "','" + salt + "')", function(err, res, fields) {
                 console.log(err);
                 console.log(salt);
             });
@@ -321,8 +323,7 @@ app.post('/home', (request, response) => {
 app.post('/starbucksnearme', (request, response) => {
     longitude = request.body.longitude;
     latitude = request.body.latitude;
-    maps.get_sturbuckses(latitude, longitude).then((response1) => {
-    });
+    maps.get_sturbuckses(latitude, longitude).then((response1) => {});
 });
 
 
@@ -337,6 +338,8 @@ app.post('/loginsearch', (request, response) => {
     if (place == ''){
         response.render('index2.hbs', {
             error: 2,
+            savedSpots: displaySaved,
+            testvar: displayText,
             coord: `<script>latitude = ${49.2827}; longitude = ${123.1207}; z = ${19};initMultPlaceMap()</script>`
         });
     }
@@ -350,7 +353,6 @@ app.post('/loginsearch', (request, response) => {
                 displaySaved += `<div id=s${i} class="favItems"><a onclick="getMap(${saved_loc[i].location_id})"> ${saved_loc[i].location_id}</a><button id="del${i}" class="delButton" onclick="deleteFav(${i})">x</button></div>`;
             }
         });
-        console.log(coordinates);
         displayText = ' ';
         if (coordinates.lat && coordinates.long) {
             maps.get_sturbuckses(coordinates.lat, coordinates.long).then((response1) => {
@@ -365,11 +367,19 @@ app.post('/loginsearch', (request, response) => {
                 });
             });
         } else {
-            response.render('index2.hbs', {
+                displaySaved = '';
+        loadUserdata(logged_in.username).then(res => {
+            console.log(saved_loc);
+            for (var i = 0; i < saved_loc.length; i++) {
+                console.log(saved_loc[i].location_id);
+                displaySaved += `<div id=s${i} class="favItems"><a onclick="getMap(${saved_loc[i].location_id})"> ${saved_loc[i].location_id}</a></div>`;
+            }
+                        response.render('index2.hbs', {
                 error: 1,
-                coord: `<script>latitude = ${49.2827}; longitude = ${123.1207}; z = ${19};initMultPlaceMap()</script>`
+                coord:`<script>latitude = ${49.2827}; longitude = ${123.1207}; z = ${19};initMultPlaceMap()</script>`,
+                savedSpots: displaySaved
             });
-
+        });
         }
     });
 });
@@ -403,8 +413,8 @@ app.post('/storeuserdata', (request, response) => {
     }
     console.log(account);
     fs.writeFileSync('accounts.json', JSON.stringify(account));*/
-    last_save = request.body.location;
     checkLocations(logged_in.username, request.body.location).then(res => {
+        last_save = request.body.location;
         addLocations(logged_in.username, request.body.location);
     }, rej => { console.log('failed'); }
     );
@@ -424,7 +434,6 @@ app.post('/favdata', (request, response) => {
             displaySaved += `<div id=s${i} class="favItems"><a onclick="getMap(${saved_loc[i].location_id})"> ${saved_loc[i].location_id}</a><button id="del${i}" class="delButton" onclick="deleteFav(${i})">x</button></div>`;
         }
          displaySaved += `<div id=s${saved_loc.length} class="favItems"><a onclick="getMap(${last_save})"> ${last_save}</a><button id="del${i}" class="delButton" onclick="deleteFav(${i})">x</button></div>`;
-
 
         current_ip.request_coodrs().then((response1) => {
             console.log(response1);
@@ -469,5 +478,5 @@ module.exports = {
     LoadAccfile,
     loadUserdata,
     checkLocations
-
 };
+
